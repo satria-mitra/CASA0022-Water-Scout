c69ff74e12e00ed0143c6d8a40fed17df5c744f8
satria-mitra <145175043+satria-mitra@users.noreply.github.com>
2024-05-22 01:53:13 +0100

    reduce reading interval to 10s

--- Arduino/DYP-A01/DYP-A01.ino
+++ Arduino/DYP-A01/DYP-A01.ino
@@ -1,5 +1,7 @@
 unsigned char data[4] = {};
 float distance;
+unsigned long previousMillis = 0; // Stores the last time data was read
+const long interval = 10000; // Interval between reads (10 seconds)
 
 void setup()
 {
@@ -9,35 +11,41 @@ void setup()
 
 void loop()
 {
-  if (Serial1.available() >= 4) // Check if there are at least 4 bytes available
-  {
-    for (int i = 0; i < 4; i++)
+  unsigned long currentMillis = millis(); // Get the current time
+
+  if (currentMillis - previousMillis >= interval) {
+    // Save the last time you read the data
+    previousMillis = currentMillis;
+
+    if (Serial1.available() >= 4) // Check if there are at least 4 bytes available
     {
-      data[i] = Serial1.read();
-    }
-    
-    if (data[0] == 0xff)
-    {
-      int sum = (data[0] + data[1] + data[2]) & 0x00FF;
-      if (sum == data[3])
+      for (int i = 0; i < 4; i++)
       {
-        distance = (data[1] << 8) + data[2];
-        if (distance > 280)
+        data[i] = Serial1.read();
+      }
+
+      if (data[0] == 0xff)
+      {
+        int sum = (data[0] + data[1] + data[2]) & 0x00FF;
+        if (sum == data[3])
         {
-          Serial.print("distance=");
-          Serial.print(distance / 10);
-          Serial.println("cm");
+          distance = (data[1] << 8) + data[2];
+          if (distance > 280)
+          {
+            Serial.print("distance=");
+            Serial.print(distance / 10);
+            Serial.println("cm");
+          }
+          else
+          {
+            Serial.println("Below the lower limit");
+          }
         }
         else
         {
-          Serial.println("Below the lower limit");
+          Serial.println("ERROR");
         }
-      }
-      else
-      {
-        Serial.println("ERROR");
       }
     }
   }
-  delay(150);
 }
